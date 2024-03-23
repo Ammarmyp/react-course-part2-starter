4cccadc84e43eb7c3f2e2a4fa50ccc82a746d949
Ammarmyp <ammarmyp@gmail.com>
2024-03-23 13:50:17 +0300

    creating complex actions and working with reducers

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+import authReducer from "./reducers/authReducer";
 
 const LoginStatus = () => {
-  const [user, setUser] = useState('');
+  // const [user, setUser] = useState('');
 
-  if (user)
+  const [value, dispatch] = useReducer(authReducer, "");
+
+  if (value)
     return (
       <>
         <div>
-          <span className="mx-2">{user}</span>
-          <a onClick={() => setUser('')} href="#">
+          <span className="mx-2">{value}</span>
+          <a onClick={() => dispatch({ type: "LOGOUT" })} href="#">
             Logout
           </a>
         </div>
@@ -16,7 +19,10 @@ const LoginStatus = () => {
     );
   return (
     <div>
-      <a onClick={() => setUser('mosh.hamedani')} href="#">
+      <a
+        onClick={() => dispatch({ type: "LOGIN", username: "mosh.hamedani" })}
+        href="#"
+      >
         Login
       </a>
     </div>
